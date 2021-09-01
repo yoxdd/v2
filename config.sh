03374391334df6ae3cdc2c09be5faf510c955d58
yoxdd <89504318+yoxdd@users.noreply.github.com>
2021-09-01 14:16:41 +0800

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# VLESS new configuration
+install -d /usr/local/etc/v2ray
+cat > /usr/local/etc/v2ray/config.json << EOF
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$ID",
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                   "path": "/$ID-vless"
+                }
+            }
+        }
+    ],
+    "outbounds": [ 
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run vless
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
